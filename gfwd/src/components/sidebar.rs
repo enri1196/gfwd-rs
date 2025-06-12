7bd4b1239388c5d5a67e7a1bfd55e5ff831fe452
Enri1196 <github@snowless.it>
2025-06-12 17:21:13 +0200

    generate and update list with factory component

--- gfwd/src/components/sidebar.rs
+++ gfwd/src/components/sidebar.rs
@@ -2,6 +2,7 @@ use relm4::adw::prelude::*;
 use relm4::prelude::*;
 use crate::fwd_broker::FwdBroker;
 
+#[tracker::track]
 #[derive(Debug, Clone, PartialEq)]
 pub struct ZoneItem {
     pub name: String,
@@ -17,6 +18,56 @@ impl From<String> for ZoneItem {
             is_default: false,
             is_active: false,
             interfaces: Vec::new(),
+            tracker: 0,
+        }
+    }
+}
+
+#[relm4::factory(pub)]
+impl FactoryComponent for ZoneItem {
+    type Init = String;
+    type Input = ();
+    type Output = ();
+    type CommandOutput = ();
+    type ParentWidget = gtk::ListBox;
+
+    view! {
+        gtk::ListBoxRow {
+            set_halign: gtk::Align::Fill,
+            set_margin_all: 4,
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Horizontal,
+                set_spacing: 12,
+                set_margin_all: 8,
+                set_hexpand: true,
+                
+                #[name(label)]
+                gtk::Label {
+                    set_label: &self.name,
+                    set_hexpand: true,
+                    set_halign: gtk::Align::Start,
+                },
+
+                #[name(default_icon)]
+                gtk::Image {
+                    set_icon_name: Some("object-select-symbolic"),
+                    #[track(self.changed(ZoneItem::is_default()))]
+                    set_visible: self.is_default,
+                    #[track(self.changed(ZoneItem::is_default()))]
+                    set_opacity: if self.is_default { 1.0 } else { 0.0 },
+                },
+            }
+        }
+    }
+
+    fn init_model(name: Self::Init, _index: &DynamicIndex, _sender: FactorySender<Self>) -> Self {
+        Self {
+            name,
+            is_default: false,
+            is_active: false,
+            interfaces: Vec::new(),
+            tracker: 0,
         }
     }
 }
@@ -25,6 +76,7 @@ impl From<String> for ZoneItem {
 pub enum InputSidebarMsg {
     UpdateZones,
     ShowAddZoneDialog,
+    SetDefaultZone,
 }
 
 #[derive(Debug)]
@@ -34,7 +86,7 @@ pub enum OutputSidebarMsg {
 
 pub struct SidebarView {
     broker: &'static FwdBroker,
-    zones: Vec<ZoneItem>,
+    zones: FactoryVecDeque<ZoneItem>,
 }
 
 #[relm4::component(async, pub)]
@@ -76,12 +128,8 @@ impl SimpleAsyncComponent for SidebarView {
                     }
                 },
 
-                append = zones_list = &gtk::ListBox {
-                    set_selection_mode: gtk::SelectionMode::None,
-                    set_css_classes: &["rich-list", "boxed-list"],
-                    set_show_separators: true,
-                    set_valign: gtk::Align::Start,
-                },
+                #[local_ref]
+                zones_list_box -> gtk::ListBox {}
             }
         }
     }
@@ -89,8 +137,20 @@ impl SimpleAsyncComponent for SidebarView {
     async fn update(&mut self, msg: Self::Input, sender: AsyncComponentSender<Self>) {
         match msg {
             InputSidebarMsg::UpdateZones => {
-                let zones_names = self.broker.get_zones().await.unwrap_or_default();
-                self.zones = zones_names.into_iter().map(ZoneItem::from).collect();
+                // let zones_names = self.broker.get_zones().await.unwrap_or_default();
+                // self.zones = zones_names.into_iter().map(ZoneItem::from).collect();
+                let zones = self.broker.get_zones().await.unwrap_or_default();
+                for zone in zones {
+                    self.zones.guard().push_back(zone);
+                }
+                sender.input(InputSidebarMsg::SetDefaultZone);
+            }
+            InputSidebarMsg::SetDefaultZone => {
+                let default_zone = self.broker.get_default_zone().await.unwrap_or_default();
+                println!("Default zone: {}", default_zone);
+                for zone in self.zones.guard().iter_mut() {
+                    zone.set_is_default(zone.name == default_zone);
+                }
             }
             InputSidebarMsg::ShowAddZoneDialog => {
                 let _ = sender.output(OutputSidebarMsg::ShowAddZoneDialog);
@@ -105,19 +165,18 @@ impl SimpleAsyncComponent for SidebarView {
     ) -> AsyncComponentParts<Self> {
         let broker = FwdBroker::get_broker().await;
 
-        let zones = broker.get_zones().await.unwrap_or_default();
-        let default_zone = broker.get_default_zone().await.unwrap_or_default();
+        let zones = FactoryVecDeque::builder()
+            .launch_default()
+            .forward(sender.input_sender(), |_| InputSidebarMsg::UpdateZones);
+        
         let model = SidebarView {
             broker,
-            zones: zones.into_iter().map(ZoneItem::from).collect(),
+            zones
         };
 
+        let zones_list_box = model.zones.widget();
         let widgets = view_output!();
-        for zone in model.zones.iter() {
-            widgets
-                .zones_list
-                .append(&gtk::Label::new(Some(&zone.name)));
-        }
+        sender.input(InputSidebarMsg::UpdateZones);
         AsyncComponentParts { model, widgets }
     }
 }
